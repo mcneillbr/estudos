d1052bb872fd67c3f2a7162f0a61736a338b98da
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-19 20:08:27 -0300

    adição de plumber

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@
  * 
  */
 'use strict';
-const { src, dest, parallel, series, watch} = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
 const del = require('delete');
+var plumber = require('gulp-plumber');
 
 const bourbon = require("bourbon").includePaths;
 const neat = require("bourbon-neat").includePaths;
@@ -17,44 +18,48 @@ const neat = require("bourbon-neat").includePaths;
 // const noop = require("gulp-noop");
 
 function cleanTask(cb) {
-    
+
     // Use the `delete` module directly, instead of using gulp-rimraf
-        del(['./dest/**/*.*'], cb);
-  }
+    del(['./dest/**/*.*'], cb);
+}
 
 function htmlTask() {
     return src('src/**/*.html')
-    .pipe(dest('./dest'));
+        .pipe(plumber())
+        .pipe(dest('./dest'));
 }
 
 function pugTask() {
-  return src('src/templates/**/*.pug')
-    .pipe(pug())
-    .pipe(dest('./dest'))
+    return src('src/templates/**/*.pug')
+        .pipe(plumber())
+        .pipe(pug())
+        .pipe(dest('./dest'))
 }
 
 function cssTask() {
     return src('./src/css/**/*.css')
-    .pipe(minifyCSS())
-    .pipe(dest('./dest/build/css'))
+        .pipe(plumber())
+        .pipe(minifyCSS())
+        .pipe(dest('./dest/build/css'))
 }
 
 function sassTask() {
-  return src('./src/css/**/*.scss')
-    .pipe(
-        sass({
-            sourcemaps: true,
-            includePaths: [bourbon, neat]
-    })
-    ).on('error', sass.logError)
-    //.pipe(minifyCSS())
-    .pipe(dest('./dest/build/css'))
+    return src('./src/css/**/*.scss')
+        .pipe(
+            sass({
+                sourcemaps: true,
+                includePaths: [bourbon, neat]
+            })
+        ).pipe(plumber())
+        //.pipe(minifyCSS())
+        .pipe(dest('./dest/build/css'))
 }
 
 function jsTask() {
-  return src('src/javascript/*.js', { sourcemaps: true })
-    .pipe(concat('app.min.js'))
-    .pipe(dest('./dest/build/js', { sourcemaps: true }))
+    return src('src/javascript/*.js', { sourcemaps: true })
+        .pipe(plumber())
+        .pipe(concat('app.min.js'))
+        .pipe(dest('./dest/build/js', { sourcemaps: true }))
 }
 
 function sassWatch() {
